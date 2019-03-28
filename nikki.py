1e5ba19204a45fd9a10dc1a089ccfcc3c1d923fc
Anh Reynolds <areynolds@Anh.attlocal.net>
2019-03-28 12:34:26 -0500

    changes to work with Python 3

--- nikki.py
+++ nikki.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import numpy as np
-import simplejson as json
+import json
 import sys
 from operator import itemgetter
 try:
@@ -141,13 +141,13 @@ for item in wardrobe:
 ##########
 
 for n in all_items:
-    assert(len(n.tags) >= 0 and len(n.tags) <= 2)
+    assert(len(list(n.tags)) >= 0 and len(list(n.tags)) <= 2)
     assert(all(tag in tag_list for tag in n.tags))
 ##    assert([n.type, n.supertype, n.subtype] in type_list) ## Fri 2/16 not passed
 
 ########## Print items with specific stats ##########
-def raw_input_list(yourlist):
-     listSTR=raw_input(yourlist)     
+def input_list(yourlist):
+     listSTR=input(yourlist)     
      listSTR =listSTR[0:len(listSTR)]
      listT = listSTR.split(",")
      listEnd=[]
@@ -156,11 +156,11 @@ def raw_input_list(yourlist):
      return listEnd
 
 ############# Getting stats for stylst arena mode ###########
-stylist_mode = str(raw_input("Stylist arena mode? (Y/N) ")).capitalize()
+stylist_mode = str(input("Stylist arena mode? (Y/N) ")).capitalize()
 manual_mode = True
 if stylist_mode == "Y" or stylist_mode == "Yes":
     manual_mode = False
-    target_theme = str(raw_input("Which theme? (e.g., The Queen, Cloud Lady, etc.) ")).capitalize()
+    target_theme = str(input("Which theme? (e.g., The Queen, Cloud Lady, etc.) ")).capitalize()
     with open("stylist_themes.json") as theme_file:
         items = json.load(theme_file)
     target_stats = []
@@ -174,90 +174,91 @@ if stylist_mode == "Y" or stylist_mode == "Yes":
                     target_stats.append(s)
                     weights_s.append(float(wt))
 
-    print "For theme", target_theme, "the target stats and weights are:"
+    print("For theme", target_theme, "the target stats and weights are:")
     for i in range(len(target_stats)):
-        print target_stats[i], weights_s[i]
+        print(target_stats[i], weights_s[i])
 else:
     ###### Ask the user for which stats we are trying to match, and verify valid input
     valid = False
     while not valid:
         valid = True
-        target_stats = raw_input_list("Stats you are looking for (e.g., Simple, Lively, Cute):  ")
+        target_stats = input_list("Stats you are looking for (e.g., Simple, Lively, Cute):  ")
         target_stats = [item for entry in target_stats for item in entry.split()]
-        target_stats = map(str.capitalize, target_stats)
+        target_stats = list(map(str.capitalize, target_stats))
         for istat in target_stats:
             if istat not in stats_list:
-                print "Requested stat not recognized: " + istat
-                print "Valid stats are as follows:  " + str(stats_list)
+                print("Requested stat not recognized: " + istat)
+                print("Valid stats are as follows:  " + str(stats_list))
                 valid = False
         if "Simple" in target_stats and "Gorgeous" in target_stats:
-            print "Simple and Gorgeous are conflicting stats; choose only one."
+            print("Simple and Gorgeous are conflicting stats; choose only one.")
             valid = False
         if "Elegance" in target_stats and "Lively" in target_stats:
-            print "Elegance and Lively are conflicting stats; choose only one."
+            print("Elegance and Lively are conflicting stats; choose only one.")
             valid = False
         if "Cute" in target_stats and "Mature" in target_stats:
-            print "Cute and Mature are conflicting stats; choose only one."
+            print("Cute and Mature are conflicting stats; choose only one.")
             valid = False
         if "Sexy" in target_stats and "Pure" in target_stats:
-            print "Sexy and Pure are conflicting stats; choose only one."
+            print("Sexy and Pure are conflicting stats; choose only one.")
             valid = False
         if "Cool" in target_stats and "Warm" in target_stats:
-            print "Cool and Warm are conflicting stats; choose only one."
+            print("Cool and Warm are conflicting stats; choose only one.")
             valid = False
 
 ## now we should have the stats either entered manually or from stylist arena database
-nstat = len(target_stats)
-print nstat, "stats: ", target_stats
+nstat = len(list(target_stats))
+print(nstat, "stats: ", target_stats)
 
 valid = False
 while not valid:
     valid = True
-    target_tags = "" if manual_mode == False else raw_input_list("Tags you are looking for (e.g., Rock, Fairy, Kimono):  ")
+    target_tags = "" if manual_mode == False else input_list("Tags you are looking for (e.g., Rock, Fairy, Kimono):  ")
     target_tags = target_tags if target_tags != [""] else []
-    target_tags = map(str.title, target_tags)
-    for i in range(0, len(target_tags)):
-        target_tags[i] = target_tags[i] if target_tags[i] != "Pop" else "POP"
-        target_tags[i] = target_tags[i] if target_tags[i] != "Republic Of China" else "Republic of China"
-        if target_tags[i] not in tag_list:
-            print "Requested tag not recognized: " + target_tags[i]
-            print "Valid tags are as follows:  " + str(tag_list)
+    target_tags = list(map(str.title, target_tags))
+    list_target_tags = list(target_tags)
+    for i in range(0, len(list_target_tags)):
+        list_target_tags[i] = list_target_tags[i] if list_target_tags[i] != "Pop" else "POP"
+        list_target_tags[i] = list_target_tags[i] if list_target_tags[i] != "Republic Of China" else "Republic of China"
+        if list_target_tags[i] not in tag_list:
+            print("Requested tag not recognized: " + list_target_tags[i])
+            print("Valid tags are as follows:  " + str(tag_list))
             valid = False
-ntag = len(target_tags)
+ntag = len(list_target_tags)
 if ntag > 0:
-    print ntag, "tags: ", target_tags
+    print(ntag, "tags: ", target_tags)
 
 valid = False
 while not valid:
     valid = True
-    item_type = str(raw_input("What are you looking for?  Name an item type or \"Outfit\":  ")).capitalize()
+    item_type = str(input("What are you looking for?  Name an item type or \"Outfit\":  ")).capitalize()
     if item_type != "" and item_type != "Outfit" and item_type not in generaltype_list:
-        print "Here are the valid choices; you can also leave it blank or specify \"Outfit\":  "
-        print generaltype_list
-        print "Requested item type not recognized: " + item_type
+        print("Here are the valid choices; you can also leave it blank or specify \"Outfit\":  ")
+        print(generaltype_list)
+        print("Requested item type not recognized: " + item_type)
         valid = False
-print "All items searched" if item_type == "" else "Type: " + item_type
+print("All items searched" if item_type == "" else "Type: " + item_type)
 
 if item_type == "Outfit":
-    newval = str(raw_input("How many items should we show for each category? (default 12) "))
+    newval = str(input("How many items should we show for each category? (default 12) "))
     nitem_display = 12 if newval == "" else int(newval)
 
-weights_t = [1.0]*len(target_tags)
+weights_t = [1.0]*len(list(target_tags))
 if manual_mode:
-    weights_s = [1.0]*len(target_stats)
-    manual_mode = str(raw_input("Scoring with different weights? (Y/N) ")).capitalize()
+    weights_s = [1.0]*len(list(target_stats))
+    manual_mode = str(input("Scoring with different weights? (Y/N) ")).capitalize()
     if manual_mode == "Y" or manual_mode == "Yes":
-        for i in range(len(target_stats)):
-            weights_s[i] = float(raw_input("Enter weight for " + target_stats[i] + ": "))
-        for i in range(len(target_tags)):
-            weights_t[i] = float(raw_input("Enter weight for " + target_tags[i] + ": "))
+        for i in range(len(list(target_stats))):
+            weights_s[i] = float(input("Enter weight for " + target_stats[i] + ": "))
+        for i in range(len(list(target_tags))):
+            weights_t[i] = float(input("Enter weight for " + target_tags[i] + ": "))
 weight_map_s = dict(zip(target_stats, weights_s))
 weight_map_t = dict(zip(target_tags, weights_t))
 
 if item_type == "Outfit":
     wrong_stats = True
 else:
-    wrong_stats = str(raw_input("Display items with some wrong stats? (Y/N) ")).capitalize()
+    wrong_stats = str(input("Display items with some wrong stats? (Y/N) ")).capitalize()
     wrong_stats = True if (wrong_stats == "Y" or wrong_stats == "Yes") else False
 
 items = []
@@ -284,21 +285,21 @@ for n in all_items:
 if (item_type != "Outfit"):
     items = sorted(items, key=itemgetter(1))
     for n in items:
-        print n[0].unique_id, n[0].title, " *** score ", n[1]
+        print(n[0].unique_id, n[0].title, " *** score ", n[1])
         for s in n[0].stats:
-            print s.stat, s.score
-        if len(n[0].tags) > 0:
-            print n[0].tags
-        print ""
-    print "Number of items with desired stats = ", len(items)
+            print(s.stat, s.score)
+        if len(list(n[0].tags)) > 0:
+            print(n[0].tags)
+        print("")
+    print("Number of items with desired stats = ", len(items))
 
 else:
     items_by_type = []
     assert(len(uniquetype_list) == len(type_list))
     for index in range(0, len(type_list)):
         i = len(type_list) - index - 1
-        print "Item type: ", uniquetype_list[i]
-        print ""
+        print("Item type: ", uniquetype_list[i])
+        print("")
         subset = []
         for j in items:
             if [j[0].type, j[0].supertype, j[0].subtype] == type_list[i]:
@@ -307,16 +308,16 @@ else:
         nprint = min(nitem_display, len(subset))
         for nindex in range(0, nprint):
             n = len(subset) - nprint + nindex
-            print subset[n][0].unique_id, subset[n][0].title, " *** score ", subset[n][1]
+            print(subset[n][0].unique_id, subset[n][0].title, " *** score ", subset[n][1])
             for s in subset[n][0].stats:
-                print s.stat, s.score
-            if len(subset[n][0].tags) > 0:
-                print subset[n][0].tags
-            print ""
-        print "Item type: ", uniquetype_list[i]
-        print ""
-        print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        print ""
+                print(s.stat, s.score)
+            if len(list(subset[n][0].tags)) > 0:
+                print(subset[n][0].tags)
+            print("")
+        print("Item type: ", uniquetype_list[i])
+        print("")
+        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        print("")
 
-print "Happy landing!"
+print("Happy landing!")
 
